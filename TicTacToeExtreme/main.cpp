2b94530d0a5d4bd113bb9ad1831bc9727915fec2
Zerklickt <zerklickt@gmail.com>
2022-07-25 16:40:53 +0200

    finished gtest setup

--- TicTacToeExtreme/main.cpp
+++ TicTacToeExtreme/main.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
+#include <iostream>
 
-int main() {
 
-	return 0;
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	//
+	
+	//
+	return RUN_ALL_TESTS();
 }

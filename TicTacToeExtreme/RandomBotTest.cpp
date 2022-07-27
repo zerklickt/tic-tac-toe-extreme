9f4e123add4f44f71063c74c291ee8136e6f61d4
Zerklickt <zerklickt@gmail.com>
2022-07-27 16:26:04 +0200

    RandomBot test added (empty)

---
+++ TicTacToeExtreme/RandomBotTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.hpp"
+
+#include "RandomBot.hpp"
+
+class PlaygroundTest : public testing::Test {
+
+protected:
+
+    RandomBot* m_randomBot;
+
+    virtual void SetUp() override
+    {
+        m_randomBot = new RandomBot('b');
+    }
+
+};
